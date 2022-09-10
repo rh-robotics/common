56a26317a540adde09139a37f33ffd09abd0d8d1
DragonDev07 <teowelton@gmail.com>
2022-09-10 17:56:18 -0600

    RUN_TO_POSITION_IMPORT

--- Hardware/HWC.java
+++ Hardware/HWC.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_TO_POSITION;
 
 import androidx.annotation.NonNull;
 
